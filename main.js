051ed1743826a74486c18c72bc8b3d82da7ffc98
Matthew Andrews <matt@mattandre.ws>
2015-07-20 16:44:15 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,22 @@
+'use strict';
+
+var catchNetworkErrors = require('./src/catch-network-errors');
+
+module.exports = function(url, opts) {
+	var retriesLeft = opts.retry === undefined ? 3 : opts.retry;
+	opts.retry = undefined;
+
+	function fetchAttempt() {
+		return fetch(url, opts)
+			.catch(catchNetworkErrors)
+			.then(function(response) {
+				if (!response.ok && retriesLeft > 0) {
+					retriesLeft--;
+					return fetchAttempt();
+				}
+				return response;
+			});
+	}
+
+	return fetchAttempt();
+};
