1e71f199892a904d6b1316a13b69369450ca39d8
Simon Legg <leggsimon@gmail.com>
2017-10-10 15:18:06 +0100

    adds support for allowed status codes (#9)
    * adds support for allowed status codes
    
    * ensure allowedStatusCodes exists

--- README.md
+++ README.md
@@ -2,4 +2,6 @@
 
 Extension of fetch which supports passing a `retry` value in to `options`. `retry` should be an integer, `n`, and a fetch will be retried `n` times (i.e. `n + 1` fetches in total) before failing.
 
+Supports an `allowedStatusCodes` value in `options`. This is an array of integers of status codes that if responded with, won't attempt to retry.
+
 Fetches also have a `stopRetrying()` method which will fulfil the promise with the reult of the current in-flight fetch and not send any more requests
--- main.js
+++ main.js
@@ -3,13 +3,19 @@ require('isomorphic-fetch');
 
 module.exports = function (url, opts) {
 	let retriesLeft = opts.retry === undefined ? 3 : opts.retry;
+	const allowedStatusCodes = opts.allowedStatusCodes;
 	opts.retry = undefined;
+	opts.allowedStatusCodes = undefined;
+
+	const isNotWhiteListed = (status) => {
+		return allowedStatusCodes && allowedStatusCodes.indexOf(status) === -1;
+	}
 
 	function fetchAttempt () {
 		const fetchCall = fetch(url, opts)
 			.catch(catchNetworkErrors)
 			.then(function (response) {
-				if (!response.ok && retriesLeft > 0) {
+				if (!response.ok && retriesLeft > 0 && isNotWhiteListed(response.status)) {
 					retriesLeft--;
 					return fetchAttempt();
 				}
